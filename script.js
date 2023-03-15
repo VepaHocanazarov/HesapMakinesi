ebc43480fcb1ebe2d1ffdedec984909e47ad4981
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-15 11:26:47 +0300

    Hesap Makinesi.

---
+++ script.js
@@ -0,0 +1,150 @@
+const display = document.querySelector(".calculater-input");
+const keys = document.querySelector(".calculater-keys");
+
+
+let displayValue = "";
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
+
+
+updateDisplay();
+
+function updateDisplay() {
+    display.value = displayValue;
+}
+
+keys.addEventListener("click", function (e) {
+    const element = e.target
+    const value = element.value
+
+    if (!element.matches("button")) return;
+
+    switch (value) {
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+        case "=":
+            handleOperator(value);
+            break;
+
+            case ".":
+                inputDecimal();
+                break;
+
+              case "clear":
+                clear();
+                break;
+                
+                default:
+                inputNumber(element.value);
+
+    }
+
+    updateDisplay();
+
+    // if (element.classList.contains("operator")) {
+
+    //     // console.log("operator", element.value);
+
+    //     handleOperator(element.value);
+    //     updateDisplay();
+    //     return;
+
+
+    // }
+    
+
+
+    // if (element.classList.contains("decimal")) {
+    //     // console.log("decimal",element.value);
+    //     inputDecimal();
+    //     updateDisplay();
+    //     return;
+    // }
+
+
+
+    // if (element.classList.contains("clear")) {
+    //     // console.log("clear",element.value);
+    //     clear();
+    //     updateDisplay();
+    //     return;
+    // }
+
+    // console.log( "number",element.value);
+
+    // inputNumber(element.value);
+    // updateDisplay();
+
+});
+
+
+function handleOperator(nextOperator) {
+
+    const value = parseFloat(displayValue);
+
+    if (operator && waitingForSecondValue) {
+        operator = nextOperator;
+        return
+    }
+
+    if (firstValue === null) {
+        firstValue = value;
+    } else if (operator) {
+        const result = calculater(firstValue, value, operator)
+
+        displayValue = `${parseFloat(result.toFixed(7))}`
+
+        firstValue = result;
+
+    }
+
+    waitingForSecondValue = true;
+    operator = nextOperator;
+
+    console.log(displayValue, operator, firstValue, waitingForSecondValue);
+
+}
+
+function calculater(first, second, operator) {
+    if (operator === "+") {
+        return first + second;
+    } else if (operator === "-") {
+        return first - second
+    } else if (operator === "*") {
+        return first * second
+    } else if (operator === "/") {
+        return first / second
+    }
+
+    return second;
+}
+
+function inputNumber(num) {
+
+    if (waitingForSecondValue) {
+        displayValue = num;
+        waitingForSecondValue = false;
+    } else {
+        displayValue = displayValue === null ? num : displayValue + num;
+
+    }
+
+    console.log(displayValue, operator, firstValue, waitingForSecondValue);
+
+};
+
+function inputDecimal() {
+
+    if (!displayValue.includes(".")) {
+        displayValue += "."
+    } else {
+        alert("Yalnış Seçim")
+    }
+}
+
+function clear() {
+    displayValue = ""
+}
